83bc6454305aef1e76ce04385e2f16eb9ebcf719
watson.zeng <watson.zeng@corp.to8to.com>
2019-10-24 14:14:15 +0800

    请求

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,56 +28,103 @@
     border: 1px solid #d6d6d6;
   }
 
-  .a_table td div {
+  .a_table td div.item {
     margin: 5px;
     padding: 5px;
     border: 1px solid #ffecec;
   }
+
+  .btn-primary {
+    display: inline-block;
+    padding: 6px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 1.42857143;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-image: none;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    color: #fff;
+    background-color: #337ab7;
+    border-color: #2e6da4;
+  }
+
+  .big-input {
+    width: 30%;
+    height: 26px;
+  }
+
+  .big-select {
+    height: 32px;
+  }
 </style>
 <template>
   <div class="hello">
-    <table class="a_table">
-      <tr>
-        <td>
-          <div>
-            <vuedraggable class="wrapper" v-model="note2" :options="{group:'people',}" @end="end">
-              <div v-for="item in note2" :key="item.id" class="item">
-                <p>{{item.name}}</p>
-              </div>
-            </vuedraggable>
-          </div>
-        </td>
-        <td>
-          <div>
-            <vuedraggable class="wrapper" v-model="note1" :options="{group:'people',}" @end="end">
-              <div v-for="item in note1" :key="item.id" class="item">
-                <p>{{item.name}}</p>
-              </div>
-            </vuedraggable>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <div>
-            <vuedraggable class="wrapper" v-model="note3" :options="{group:'people',}" @end="end">
-              <div v-for="item in note3" :key="item.id" class="item">
-                <p>{{item.name}}</p>
-              </div>
-            </vuedraggable>
-          </div>
-        </td>
-        <td>
-          <div>
-            <vuedraggable class="wrapper" v-model="note4" :options="{group:'people',}" @end="end">
-              <div v-for="item in note4" :key="item.id" class="item">
-                <p>{{item.name}}</p>
-              </div>
-            </vuedraggable>
-          </div>
-        </td>
-      </tr>
-    </table>
+    <div class="hello">
+      <ul>
+        <form @submit.prevent="submit">
+          <select class="big-select" name="public-choice" v-model="form_data.selected">
+            <option :value="coupon.id" v-for="coupon in type_list" v-bind:key="coupon.id">{{coupon.name}}</option>
+          </select>
+          <input class="big-input" type="text" name="name" v-model="form_data.a_task">
+          <input class="btn-primary" type="submit" value="提交">
+        </form>
+      </ul>
+    </div>
+    <div>
+      <table class="a_table">
+        <tr>
+          <td>
+            <div>
+              <vuedraggable class="wrapper" v-model="note[2]" :options="{group:'people',}" @end="end">
+                <div v-for="item in note[2]" :key="item.id" class="item">
+                  <p>{{item.name}}</p>
+                </div>
+              </vuedraggable>
+            </div>
+          </td>
+          <td>
+            <div>
+              <vuedraggable class="wrapper" v-model="note[1]" :options="{group:'people',}" @end="end">
+                <div v-for="item in note[1]" :key="item.id" class="item">
+                  <p>{{item.name}}</p>
+                </div>
+              </vuedraggable>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div>
+              <vuedraggable class="wrapper" v-model="note[3]" :options="{group:'people',}" @end="end">
+                <div v-for="item in note[3]" :key="item.id" class="item">
+                  <p>{{item.name}}</p>
+                </div>
+              </vuedraggable>
+            </div>
+          </td>
+          <td>
+            <div>
+              <vuedraggable class="wrapper" v-model="note[4]" :options="{group:'people',}" @end="end">
+                <div v-for="item in note[4]" :key="item.id" class="item">
+                  <p>{{item.name}}</p>
+                </div>
+              </vuedraggable>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </div>
     <div>
       删除区域
       <vuedraggable class="wrapper" v-model="trash" :options="{group:'people',}" @end="end">
@@ -94,62 +141,71 @@
     name: 'hello',
     data () {
       return {
-        info: null,
+        type_list: [
+          {'id': 1, 'name': '重紧'},
+          {'id': 2, 'name': '重不紧'},
+          {'id': 3, 'name': '不重不紧'},
+          {'id': 4, 'name': '不重紧'}
+        ],
+        form_data: {
+          selected: '',
+          a_task: ''
+        },
         trash: [],
-        note1: [
-          {
-            id: 1,
-            name: 'boss迁OA'
-          },
-          {
-            id: 2,
-            name: '目标计划'
-          }
-        ],
-        note2: [
-          {
-            id: 3,
-            name: '学习mybatis'
-          },
-          {
-            id: 4,
-            name: '学习vue,go'
-          },
-          {
-            id: 5,
-            name: '跑步'
-          }
-        ],
-        note3: [
-          {
-            id: 6,
-            name: '玩游戏'
-          },
-          {
-            id: 7,
-            name: '弹琴'
-          }
-        ],
-        note4: [
-          {
-            id: 8,
-            name: '吃午饭'
-          },
-          {
-            id: 9,
-            name: '告警'
-          }
-        ]
+        note: {
+          1: [
+            {
+              id: 1,
+              name: 'boss迁OA'
+            },
+            {
+              id: 2,
+              name: '目标计划'
+            }
+          ],
+          2: [
+            {
+              id: 3,
+              name: '学习mybatis'
+            },
+            {
+              id: 4,
+              name: '学习vue,go'
+            },
+            {
+              id: 5,
+              name: '跑步'
+            }
+          ],
+          3: [
+            {
+              id: 6,
+              name: '玩游戏'
+            },
+            {
+              id: 7,
+              name: '弹琴'
+            }
+          ],
+          4: [
+            {
+              id: 8,
+              name: '吃午饭'
+            },
+            {
+              id: 9,
+              name: '告警'
+            }
+          ]
+        }
       }
     },
     mounted () {
-      // this.axios.post('http://192.168.3.97:9999/form', {
-      //   username: 'Fred',
-      //   password: 'Flintstone'
-      // }).then(response => (this.info = response.data))
-      //   .catch(function (error) { // 请求失败处理
-      //     console.log(error)
-      //   })
+      // 多一个斜线就不一样了，会301， http://192.168.3.97:9999/tasks/
+      this.axios.get('http://192.168.3.97:9999/tasks').then(response => (this.info = response.data))
+        .catch(function (error) { // 请求失败处理
+          console.log(error)
+        })
     },
     updated () {
       console.log('note1', this.note1)
@@ -159,6 +215,11 @@
     methods: {
       end (ev) {
         console.log(ev.to.className)
+      },
+      submit: function () {
+        console.log(this.form_data)
+        this.note[this.form_data.selected].push({'id': '', 'name': this.form_data.a_task})
+        this.form_data.a_task = ''
       }
     }
   }
