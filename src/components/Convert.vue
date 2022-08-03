49cdea3cdddadc6cc1834a33715b187d76522636
zenghuasheng <zenghuasheng@ones.ai>
2022-08-03 10:40:49 +0800

    请求参数转换

--- src/components/Convert.vue
+++ src/components/Convert.vue
@@ -1,25 +1,14 @@
 <template>
   <el-form ref="form" :model="form" label-width="80px">
-    <el-form-item label="天机阁tags">
-      <el-input autosize type="textarea" size="medium" v-model="form.tags"></el-input>
-      <el-input type="textarea" v-model="form.tagsRes"></el-input>
-      <el-button type="primary" @click="onSubmit('tags', form.tags)">转换</el-button>
+    <el-form-item label="参数转换">
+      <el-button type="primary" @click="graphqlConvert()">转换</el-button>
+      <el-input autosize type="textarea" size="medium" v-model="form.graphql"></el-input>
     </el-form-item>
-    <el-form-item label="文件ID解码">
-      <el-input autosize type="textarea" size="medium" v-model="form.fileID"></el-input>
-      <el-input type="textarea" v-model="form.fileIDRes"></el-input>
-      <el-button type="primary" @click="onSubmit('fileID', form.fileID)">转换</el-button>
-    </el-form-item>
-    <el-form-item label="转义">
+    <!-- <el-form-item label="转义">
       <el-input autosize type="textarea" size="medium" v-model="form.url"></el-input>
       <el-input type="textarea" v-model="form.urlRes"></el-input>
       <el-button type="primary" @click="onSubmit('rep', form.url)">转换</el-button>
-    </el-form-item>
-    <el-form-item label="oidb命令字转换">
-      <el-input autosize type="textarea" size="medium" v-model="form.oidb"></el-input>
-      <el-input type="textarea" v-model="form.oidbRes"></el-input>
-      <el-button type="primary" @click="onSubmit('oidb', form.oidb)">转换</el-button>
-    </el-form-item>
+    </el-form-item> -->
   </el-form>
 </template>
 <script>
@@ -28,6 +17,7 @@ export default {
   data() {
     return {
       form: {
+        graphql: '',
         tags: '',
         tagsRes: '',
         fileID: '',
@@ -45,6 +35,9 @@ export default {
         console.log(data)
         if (data.data.data) {
           switch (type) {
+            case 'graphql':
+              this.form.graphql = data.data.data
+              break
             case 'tags':
               this.form.tagsRes = data.data.data
               break
@@ -66,34 +59,18 @@ export default {
         console.log(error)
       })
     },
-    localConvert() {
-      if (this.form.desc.length <= 0) {
+    graphqlConvert() {
+      if (this.form.graphql.length <= 0) {
         return
       }
-      let s = this.form.desc
-      let ss = s.split('\n')
-      let sWithPrefix = []
-      for (let i = 0; i < ss.length; i++) {
-        if (i % 2 === 0) {
-          ss[i] = 'tags.' + ss[i]
-        }
-        ss[i] = ss[i].trim()
-        sWithPrefix.push(ss[i])
+      let s = this.form.graphql
+      if (s.indexOf('\\n') >= 0) {
+        let strList = s.split('\\n')
+        this.form.graphql = strList.join('\n')
+        return
       }
-      console.log(sWithPrefix)
-      let sHaveColon = []
-      for (let i = 0; i < sWithPrefix.length; i++) {
-        if (i % 2 === 0) {
-          let newS = sWithPrefix[i] + ':' + sWithPrefix[i + 1]
-          if (sWithPrefix[i] === 'tags.trpc.status_code' || sWithPrefix[i] === 'tags.result_code') {
-            sHaveColon.push('_exists_:"' + sWithPrefix[i] + '"')
-          }
-          sHaveColon.push(newS)
-        }
-      }
-      console.log(sHaveColon)
-      console.log(sHaveColon.join(' AND '))
-      this.form.result = sHaveColon.join(' AND ')
+      let strList = s.split('\n')
+      this.form.graphql = strList.join('\\n')
     }
   }
 }
