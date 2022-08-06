08ea273d14575d37b559a50e7e35a54a29d24678
zenghuasheng <zenghuasheng@ones.ai>
2022-08-06 18:01:37 +0800

    sql转换

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 参考https://www.runoob.com/vue2/vue-install.html
 
+淘宝的源
+```shell
+brew install node
+npm config set registry https://registry.npm.taobao.org
+
+source ~/.bash_profile
+/opt/lib/node_modules/cnpm/bin/cnpm
+/opt/lib/node_modules/cnpm/bin/cnpm install
+/opt/lib/node_modules/cnpm/bin/cnpm run dev
+cnpm run build
+cp -rf dist/index.html /Users/xhs/go_workspace/work-utils-backend/views/index.html
+cp -rf dist/static /Users/xhs/go_workspace/work-utils-backend/static
+cp -rf dist/static/* /Users/xhs/go_workspace/work-utils-backend/static
+mkdir dist
+cp work-utils-backend dist
+cp -rf views dist
+cp -rf static dist
+ls dist
+cp -rf conf dist
+tar -zcvf dist.tar.gz dist
+scp dist.tar.gz root@xhs:/usr/local/openresty/nginx/html/
+scp dist.tar.gz root@114.215.111.84:/usr/local/openresty/nginx/html/ 
+```
 1. 安装
 
    ```
--- src/components/Convert.vue
+++ src/components/Convert.vue
@@ -1,14 +1,14 @@
 <template>
   <el-form ref="form" :model="form" label-width="80px">
-    <el-form-item label="参数转换">
+    <el-form-item label="item/graphql参数转换">
       <el-button type="primary" @click="graphqlConvert()">转换</el-button>
       <el-input autosize type="textarea" size="medium" v-model="form.graphql"></el-input>
     </el-form-item>
-    <!-- <el-form-item label="转义">
-      <el-input autosize type="textarea" size="medium" v-model="form.url"></el-input>
-      <el-input type="textarea" v-model="form.urlRes"></el-input>
-      <el-button type="primary" @click="onSubmit('rep', form.url)">转换</el-button>
-    </el-form-item> -->
+    <el-form-item label="sql填充参数">
+      <el-input autosize :placeholder="sqlPlaceholder" type="textarea" size="medium" v-model="form.sql"></el-input>
+      <el-input autosize type="textarea" v-model="form.sqlRes"></el-input>
+      <el-button type="primary" @click="onSubmit('sql', form.sql)">转换</el-button>
+    </el-form-item>
   </el-form>
 </template>
 <script>
@@ -16,6 +16,7 @@ export default {
   name: 'Convert',
   data() {
     return {
+      sqlPlaceholder: ' [SQL] 2022/08/04 16:04:42 SELECT xxx ? ?; [1:"xxx" 2:"xxx" 3:1]',
       form: {
         graphql: '',
         tags: '',
@@ -24,6 +25,8 @@ export default {
         fileIDRes: '',
         url: '',
         urlRes: '',
+        sql: '',
+        sqlRes: '',
         oidb: '',
         oidbRes: ''
       }
@@ -31,10 +34,13 @@ export default {
   },
   methods: {
     onSubmit(type, value) {
-      this.axios.post('http://localhost:9090/convert', {'type': type, 'value': value}).then(data => {
+      this.axios.post('http://task.xiaohuasheng.cc/api/convert', {'type': type, 'value': value}).then(data => {
         console.log(data)
         if (data.data.data) {
           switch (type) {
+            case 'sql':
+              this.form.sqlRes = data.data.data
+              break
             case 'graphql':
               this.form.graphql = data.data.data
               break
@@ -52,7 +58,7 @@ export default {
               break
           }
         } else {
-          this.$message('更新失败')
+          this.$message('转换失败')
         }
       }).catch(function (error) {
         // 请求失败处理
