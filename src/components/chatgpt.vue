38f8993f147816a8e277d37074e6ac78e319a53c
zenghuasheng <zenghuasheng@ones.ai>
2023-08-10 22:33:45 +0800

    add: 存微信公众号cookie

--- src/components/chatgpt.vue
+++ src/components/chatgpt.vue
@@ -2,6 +2,7 @@
   <el-form ref="form" label-width="80px">
     <el-form-item label="chatgpt">
       <div>https://chat.openai.com/api/auth/session</div>
+      <el-input type="input" size="medium" v-model="textType"></el-input>
       <el-input autosize type="textarea" size="medium" v-model="chatgpt"></el-input>
       <el-button type="primary" @click="onSubmit">保存</el-button>
     </el-form-item>
@@ -13,12 +14,12 @@ export default {
   data() {
     return {
       chatgpt: '',
-      chatgptRes: ''
+      textType: 'curl'
     }
   },
   methods: {
     onSubmit() {
-      let param = {'content': this.chatgpt}
+      let param = {'content': this.chatgpt, 'text_type': this.textType}
       this.axios.post('http://task.xiaohuasheng.cc/api/chatgpt', param).then(data => {
         if (data.data.code === 0) {
           this.$message('保存成功')
