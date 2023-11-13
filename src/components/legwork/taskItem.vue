8922923fe67a9fd8ab4f4b641d2da7067a078ec5
zenghuasheng <zenghuasheng@ones.ai>
2023-11-13 10:09:51 +0800

    add: 修改领取按钮

--- src/components/legwork/taskItem.vue
+++ src/components/legwork/taskItem.vue
@@ -8,25 +8,8 @@
     align-items: center;
     height: 20px;
 "><a class="time"><span>{{ think.createTime }}</span></a>
-          <div @click="acceptTask(think)" class="tools" style="
-    width: 16px;
-    height: 16px;
-    fill: currentcolor;
-"><span><span class="el-popover__reference-wrapper">
-            <button aria-describedby="el-popover-2646"
-                    tabindex="0"
-                    class="el-popover__reference" style="
-    cursor: pointer;
-    border: none;
-    background: transparent;
-    color: #7d7d7d;
-    font-size: 14px;
-">
-<!--              https://iconsvg.xyz/-->
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#25c911" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><polyline points="17 11 19 13 23 9"></polyline></svg>
-            </button>
-          </span>
-          </span>
+          <div style="margin-right: 1rem">
+            <el-button round size="mini" @click="acceptTask(think)">领取</el-button>
           </div>
         </div>
         <div class="content">
@@ -34,7 +17,7 @@
           <div class="richText" style="max-height: none;"><p>ID: {{ think.taskID}}</p></div>
           <div class="richText" style="max-height: none;"><p>起点: {{ think.startingPoint }}</p></div>
           <div class="richText" style="max-height: none;"><p>终点: {{ think.endPoint }}</p></div>
-          <div class="richText" style="max-height: none;"><p>奖励: {{ think.reward }}<svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" viewBox="0 0 24 24" fill="none" stroke="#f5e505" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg></p></div>
+          <div class="richText" style="max-height: none;"><p>奖励: {{ think.reward }} <i class="el-icon-coin"></i></p></div>
         </div>
       </div>
     </div>
